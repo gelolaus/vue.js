c522abe46dbd4c02d64a65daf31fb3b3f8208471
Angelo Laus <github@alias.gelolaus.com>
2026-01-13 14:48:14 +0800

    feat(components): Implement FoodItem component with click counter and styles
    Introduces a new FoodItem component displaying a food name, message, and click counter. Updates App.vue to render multiple FoodItem components and applies new styles. Registers FoodItem globally in main.js.

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,18 @@
-<script>
-
-</script>
-
 <template>
-  
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
-<style>
+<script></script>
 
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
 </style>
